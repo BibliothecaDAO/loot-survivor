b284eda5805860d2ff285a1898ea4c51bce1b8bf
starknetdev <starknetdev@gmail.com>
2023-04-28 16:35:11 +0100

    update addresses

--- ui/src/app/hooks/useContracts.tsx
+++ ui/src/app/hooks/useContracts.tsx
@@ -11,7 +11,7 @@ export const useContracts = () => {
     address:
       (account as any)?.baseUrl ==
       "http://survivor-indexer.bibliothecadao.xyz:5050"
-        ? "0x07a6771c44602b93e8375610f886cefde0e3752073a5761b0fe2566281debf8e"
+        ? "0x009056f46cb33d53c479b1d7c063c196bcd4e3549d58c9c9555b8bd2fea5ff4d"
         : "0x035d755a23ec72df90819f584d9a1849bbc21fa77f96d25e03f1736883895248",
     abi: Adventurer,
   });
@@ -20,7 +20,7 @@ export const useContracts = () => {
     address:
       (account as any)?.baseUrl ==
       "http://survivor-indexer.bibliothecadao.xyz:5050"
-        ? "0x04fd9fd16370c0dd2c562cb7aa2d6b973142da85c0cfb740ea69841009e98380"
+        ? "0x0216071afae3defb274a33c6de4b5480fe3a4dd40c4832f69363b65f1ccd4614"
         : "0x065669e15c8f1a7f17b7062e4eb1b709b922b931b93c59577f1848a85c30ab1f",
     abi: LootMarketArcade,
   });
@@ -29,7 +29,7 @@ export const useContracts = () => {
     address:
       (account as any)?.baseUrl ==
       "http://survivor-indexer.bibliothecadao.xyz:5050"
-        ? "0x03b2879fae55706b2412c7798570877b0e42712f7010910bb7c92912b0c6c7f3"
+        ? "0x028298b791b7d938a063be38799ca1e217afc9a18e2af085031920f50cd4e006"
         : "0x000f4dbfe5d15792aa91025e42ee1d74c22bdeb1eef0b9bc19a37216377290c1",
     abi: Beast,
   });
@@ -38,7 +38,7 @@ export const useContracts = () => {
     address:
       (account as any)?.baseUrl ==
       "http://survivor-indexer.bibliothecadao.xyz:5050"
-        ? "0x00d150f39c3e69444d7e9f54ce8fa735807b2438c5e510c255187b5f9c23ff05"
+        ? "0x025cc653405372411731fc0d187d14897db48be93609120beb18fdbbf547375b"
         : "0x023b86be0b3da5c2fdbd80d1d57f1b54391588ba338acecdd014a208d47ba9ca",
     abi: Lords_ERC20_Mintable,
   });
