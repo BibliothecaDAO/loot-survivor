b8e7c0e5b0a1d0b9d27ba2739bd8c7bed5f9648c
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-05-01 10:07:46 +1000

    styles

--- ui/src/app/components/ItemDisplay.tsx
+++ ui/src/app/components/ItemDisplay.tsx
@@ -1,7 +1,7 @@
 export const ItemDisplay = (item: any) => {
     const formatItem = item.item;
     return (
-        <div className="flex flex-row gap-2 p-2 border border-terminal-green">
+        <div className={`flex flex-row gap-2 p-2 border border-terminal-green  ${formatItem ? "bg-terminal-green text-terminal-black" : ""}`}>
             <p className="whitespace-nowrap">
                 {formatItem ? formatItem.item : "Nothing Equipped"}
             </p>
