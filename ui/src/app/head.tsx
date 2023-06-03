1cdc6d829e1dee0867221ac32212da9fabcbc141
spaghettiOnToast <starknetgg@gmail.com>
2023-06-03 15:11:39 +0200

    head change

--- ui/src/app/head.tsx
+++ ui/src/app/head.tsx
@@ -1,12 +1,12 @@
 export default function Head() {
   return (
     <>
-      <title>Loot Survivor 🪦 </title>
+      <title>Loot Survivor 💀 </title>
 
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link
         rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💀</text></svg>"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪦</text></svg>"
       />
     </>
   );
