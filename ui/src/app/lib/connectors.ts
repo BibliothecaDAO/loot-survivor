c29093a7d40db8c5b5342fbf5bb8049c65a2203b
spaghettiOnToast <starknetgg@gmail.com>
2023-06-04 15:02:14 +0200

    temporarily remove catridge

--- ui/src/app/lib/connectors.ts
+++ ui/src/app/lib/connectors.ts
@@ -2,48 +2,48 @@ import { InjectedConnector } from "@starknet-react/core";
 import ControllerConnector from "@cartridge/connector";
 import { contracts } from "../hooks/useContracts";
 
-export const controllerConnector = new ControllerConnector([
-    {
-        target: contracts.goerli.lords_erc20_mintable,
-        method: "mint",
-    },
-    {
-        target: contracts.goerli.lords_erc20_mintable,
-        method: "approve",
-    },
-    {
-        target: contracts.goerli.adventurer,
-        method: "mint_with_starting_weapon",
-    },
-    {
-        target: contracts.goerli.adventurer,
-        method: "explore"
-    },
-    {
-        target: contracts.goerli.adventurer,
-        method: "upgrade_stat"
-    },
-    {
-        target: contracts.goerli.lootMarketArcade,
-        method: "claim_item"
-    },
-    {
-        target: contracts.goerli.lootMarketArcade,
-        method: "mint_daily_items"
-    },
-    {
-        target: contracts.goerli.lootMarketArcade,
-        method: "bid_on_item"
-    },
-    {
-        target: contracts.goerli.beast,
-        method: "attack"
-    },
-    {
-        target: contracts.goerli.beast,
-        method: "flee"
-    }
-]);
+// export const controllerConnector = new ControllerConnector([
+//     {
+//         target: contracts.goerli.lords_erc20_mintable,
+//         method: "mint",
+//     },
+//     {
+//         target: contracts.goerli.lords_erc20_mintable,
+//         method: "approve",
+//     },
+//     {
+//         target: contracts.goerli.adventurer,
+//         method: "mint_with_starting_weapon",
+//     },
+//     {
+//         target: contracts.goerli.adventurer,
+//         method: "explore"
+//     },
+//     {
+//         target: contracts.goerli.adventurer,
+//         method: "upgrade_stat"
+//     },
+//     {
+//         target: contracts.goerli.lootMarketArcade,
+//         method: "claim_item"
+//     },
+//     {
+//         target: contracts.goerli.lootMarketArcade,
+//         method: "mint_daily_items"
+//     },
+//     {
+//         target: contracts.goerli.lootMarketArcade,
+//         method: "bid_on_item"
+//     },
+//     {
+//         target: contracts.goerli.beast,
+//         method: "attack"
+//     },
+//     {
+//         target: contracts.goerli.beast,
+//         method: "flee"
+//     }
+// ]);
 
 export const argentConnector = new InjectedConnector({
     options: {
@@ -58,4 +58,5 @@ export const braavosConnector = new InjectedConnector({
 });
 
 
-export const connectors = [controllerConnector as any, argentConnector, braavosConnector];
+// export const connectors = [controllerConnector as any, argentConnector, braavosConnector];
+export const connectors = [argentConnector, braavosConnector]
