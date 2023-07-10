61324e165f29592be6d2f7af4fb507bfd602a57a
spaghettiOnToast <starknetgg@gmail.com>
2023-07-10 17:03:54 +0200

    tutorials

--- ui/src/app/components/tutorial/AdventurerTutorial.tsx
+++ ui/src/app/components/tutorial/AdventurerTutorial.tsx
@@ -4,17 +4,24 @@ export const AdventurerTutorial = () => {
       <h1 className="mt-0">Welcome to Loot Survivor</h1>
       <h3 className="text-xl sm:text-2xl">Create an Adventurer</h3>
       <p className="text-sm sm:text-lg">
-        Adventurers are the gate to playing Loot Survivor. You must create one
-        with a unique name, realm id, and choose a starting weapon. This will
-        follow you through your game journey until you die.
+        Hello there, fellow explorer! Welcome to the thrilling world of Loot
+        Survivor. To kickstart your epic adventure, you will first need to
+        create your very own Adventurer. Think of it as stepping into a pair of
+        sturdy boots that will carry you across the vast expanses of our game
+        world until the very end.
       </p>
       <p className="text-sm sm:text-lg">
-        Select your adventurer&apos;s persona. Remember, there is no limit to
-        the number of adventurers you can spawn!
+        To bring your Adventurer to life, you&apos;ll need a unique name, a home
+        Realm, Order and Divinity, race and of course, your initial weapon of
+        choice. This will be your identity throughout your journey. And the best
+        part? There&apos;s absolutely no limit to the number of Adventurers you
+        can conjure up!
       </p>
       <p className="text-sm sm:text-lg">
-        Once your adventurer is minted you will be able to make several actions
-        in order to rank up1
+        Once your Adventurer is minted, a world of opportunities opens up.
+        You'll be able to perform a range of actions to help them climb the
+        ranks and achieve the glory they deserve. So, are you ready to Loot
+        Survivor? Let's get adventuring!
       </p>
     </div>
   );
