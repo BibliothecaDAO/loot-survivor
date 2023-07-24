59f1b21e780531e99696fd7e0f16c655d157cdf8
starknetdev <starknetdev@gmail.com>
2023-07-24 01:01:31 +0100

    update game contract

--- ui/src/app/lib/constants.ts
+++ ui/src/app/lib/constants.ts
@@ -33,7 +33,7 @@ export const contracts = {
       "0x067e87cea28bfd9314a1d3c41fb26a58ca1346ff0ea2452e59b9eeb2828692dc",
   },
   goerli: {
-    game: "0x0153d1e01c42a0a7b9a41f32359a79c272aae8fdf86b6eab9eede1274bdde833",
+    game: "0x040d924dd15ab1cfd38cc1d1a4c7642ae75d379dfefbec68b6acf94906c59827",
     lords_erc20_mintable:
       "0x059dac5df32cbce17b081399e97d90be5fba726f97f00638f838613d088e5a47",
   },
