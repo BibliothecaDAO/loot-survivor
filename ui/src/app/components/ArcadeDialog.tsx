c5a00edc717c6b224ea5797e865c03cd3e19acc4
ponderingdemocritus <ponderingdemocritus@protonmail.com>
2023-07-31 07:05:14 +1000

    wallet work

--- ui/src/app/components/ArcadeDialog.tsx
+++ ui/src/app/components/ArcadeDialog.tsx
@@ -3,15 +3,16 @@ import { Button } from "./buttons/Button";
 import { useBurner } from "../lib/burner";
 import { useAccount, useConnectors } from "@starknet-react/core";
 import { ArcadeConnector } from "../lib/arcade";
+import { useCallback } from "react";
 
 export const ArcadeDialog = () => {
   const { address, account } = useAccount();
   const showArcadeDialog = useUIStore((state) => state.showArcadeDialog);
   const arcadeDialog = useUIStore((state) => state.arcadeDialog);
   const { connect } = useConnectors();
-  const { create, get, list } = useBurner()
+  const { create, get, list, isDeploying } = useBurner()
 
-  const arcadeAccounts = () => {
+  const arcadeAccounts = useCallback(() => {
     const arcadeAccounts = [];
     const burners = list();
 
@@ -26,37 +27,46 @@ export const ArcadeDialog = () => {
     }
 
     return arcadeAccounts;
-  };
+  }, [account, isDeploying]);
 
-
+  const onConnect = useCallback((account: ArcadeConnector) => {
+    console.log(account)
+    connect(account)
+  }, [isDeploying]);
 
   return (
     <>
       <div className="fixed inset-0 opacity-80 bg-terminal-black z-40" />
       <div className="fixed text-center top-1/8 left-1/8 sm:left-1/4 w-3/4 sm:w-1/2 h-3/4 border  bg-terminal-black z-50 border-terminal-green">
+
         <Button onClick={() => showArcadeDialog(!arcadeDialog)}>close</Button>
+
         <h3 className="mt-8">Arcade Account</h3>
-        <p className="text-2xl pb-12">Create an Arcade Account here to allow for signature free gameplay.</p>
 
-        {((account?.signer as any)?.pk == '0x0') && (<Button onClick={() => create()}>
-          create arcade account
-        </Button>)}
+        <p className="text-2xl pb-8">Create an Arcade Account here to allow for signature free gameplay!</p>
 
+        {((account?.signer as any)?.pk == '0x0') && (
+          <div>
+            <Button onClick={() => create()}>
+              create arcade account
+            </Button>
+            <p className="my-2 text-terminal-yellow">Note: This will initiate a 0.01 ETH transaction to the new account.</p>
+          </div>
+        )}
 
         <hr className="my-4 border-terminal-green" />
 
-        <h5>Existing Arcade Accounts</h5>
+        <h5>Existing</h5>
+
         <div className="flex gap-2 flex-col w-48 p-4 mx-auto">
           {arcadeAccounts().map((account, index) => {
             const connected = address == account.name;
-            return <Button variant={connected ? 'default' : 'outline'} key={index} onClick={() => connect(account)}>
+            return <Button variant={connected ? 'default' : 'outline'} key={index} onClick={() => onConnect(account)}>
               {connected ? 'connected' : 'connect'}  {account.id}
             </Button>
           })}
         </div>
-
-
-
+        {isDeploying && (<p>Deploying...</p>)}
       </div>
     </>
   );
--- ui/src/app/components/intro/WalletSelect.tsx
+++ ui/src/app/components/intro/WalletSelect.tsx
@@ -1,10 +1,6 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Button } from "../buttons/Button";
 import { useConnectors, useAccount } from "@starknet-react/core";
-import {
-  AddDevnetButton,
-  SwitchToDevnetButton,
-} from "../archived/DevnetConnectors";
 import useUIStore from "../../hooks/useUIStore";
 import Image from "next/image";
 
@@ -15,9 +11,6 @@ interface WalletSelectProps {
 const WalletSelect = ({ screen }: WalletSelectProps) => {
   const { connectors, connect } = useConnectors();
   const { account } = useAccount();
-
-  console.log(account)
-  const [addedDevnet, setAddedDevnet] = useState<boolean>(false);
   const setConnected = useUIStore((state) => state.setConnected);
 
   useEffect(() => {
@@ -37,6 +30,9 @@ const WalletSelect = ({ screen }: WalletSelectProps) => {
     }
   }, [account, screen, setConnected]);
 
+  const arcadeConnectors = () => connectors.filter((connector) => connector.name.includes("0x"));
+  const walletConnectors = () => connectors.filter((connector) => !connector.name.includes("0x"));
+
   return (
     <div className="flex flex-col p-8">
       <div className="flex flex-col self-center my-auto">
@@ -53,52 +49,28 @@ const WalletSelect = ({ screen }: WalletSelectProps) => {
           <h1 className="mb-10">The Hour for Survival Has Arrived</h1>
         </div>
 
-        {screen == 4 ? (
-          <div className="flex flex-col w-1/2 gap-5 m-auto">
-            {connectors.length > 0 ? (
-              connectors.map((connector, index) => (
-                <Button
-                  onClick={() => connect(connector)}
-                  key={index}
-                  className="w-full"
-                >
-                  Connect {connector.id}
-                </Button>
-              ))
-            ) : (
-              <h1>You must have Argent or Braavos installed!</h1>
-            )}
-          </div>
-        ) : (
-          <div className="flex flex-col w-1/2 gap-5 m-auto">
-            {connectors.some(
-              (connector: any) => connector.id() == "argentX"
-            ) ? (
-              connectors.map((connector, index) => (
-                <>
-                  {connector.id == "argentX" ? (
-                    <Button
-                      onClick={() => connect(connector)}
-                      key={index}
-                      className="w-full"
-                      disabled={account ? true : false}
-                    >
-                      Connect {connector.id}
-                    </Button>
-                  ) : null}
-                </>
-              ))
-            ) : (
-              <h1>To use devnet you must have an Argent wallet!</h1>
-            )}
-            <AddDevnetButton
-              // isDisabled={!account?.address}
-              isDisabled={addedDevnet}
-              setAddDevnet={setAddedDevnet}
-            />
-            <SwitchToDevnetButton isDisabled={false} />
-          </div>
-        )}
+
+        <div className="flex flex-col w-1/2 gap-5 m-auto">
+          {walletConnectors().map((connector, index) => (
+              <Button
+                onClick={() => connect(connector)}
+                key={index}
+                className="w-full"
+              >
+                Connect {connector.id}
+              </Button>
+            ))}
+        <h5 className="text-center">Arcade Accounts</h5>
+          {arcadeConnectors().map((connector, index) => (
+              <Button
+                onClick={() => connect(connector)}
+                key={index}
+                className="w-full"
+              >
+                Connect {connector.id}
+              </Button>
+            ))}
+        </div>
       </div>
     </div>
   );
