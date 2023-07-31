4376d108e2a20398f942e02cac5e05cba491ec23
starknetdev <starknetdev@gmail.com>
2023-07-31 15:38:39 +0100

    update apibara

--- ui/src/app/template.tsx
+++ ui/src/app/template.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { StarknetConfig } from "@starknet-react/core";
@@ -8,55 +8,52 @@ import { connectors } from "./lib/connectors";
 import { useEffect, useState } from "react";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-    const client = new ApolloClient({
-        uri: getGraphQLUrl,
-        cache: new InMemoryCache({
-            typePolicies: {
-                Query: {
-                    fields: {
-                        discoveries: {
-                            merge(existing = [], incoming) {
-                                const incomingTxHashes = new Set(
-                                    incoming.map((i: any) => i.txHash)
-                                );
-                                const filteredExisting = existing.filter(
-                                    (e: any) => !incomingTxHashes.has(e.txHash)
-                                );
-                                return [...filteredExisting, ...incoming];
-                            },
-                        },
-                        battles: {
-                            merge(existing = [], incoming) {
-                                const incomingTxHashes = new Set(
-                                    incoming.map((i: any) => i.txHash)
-                                );
-                                const filteredExisting = existing.filter(
-                                    (e: any) => !incomingTxHashes.has(e.txHash)
-                                );
-                                return [...filteredExisting, ...incoming];
-                            },
-                        },
-                    },
-                },
+  const client = new ApolloClient({
+    uri: getGraphQLUrl,
+    cache: new InMemoryCache({
+      typePolicies: {
+        Query: {
+          fields: {
+            discoveries: {
+              merge(existing = [], incoming) {
+                const incomingTxHashes = new Set(
+                  incoming.map((i: any) => i.txHash)
+                );
+                const filteredExisting = existing.filter(
+                  (e: any) => !incomingTxHashes.has(e.txHash)
+                );
+                return [...filteredExisting, ...incoming];
+              },
             },
-        }),
-    });
+            battles: {
+              merge(existing = [], incoming) {
+                const incomingTxHashes = new Set(
+                  incoming.map((i: any) => i.txHash)
+                );
+                const filteredExisting = existing.filter(
+                  (e: any) => !incomingTxHashes.has(e.txHash)
+                );
+                return [...filteredExisting, ...incoming];
+              },
+            },
+          },
+        },
+      },
+    }),
+  });
 
-    const { arcadeAccounts, isDeploying } = useBurner();
+  const { arcadeAccounts, isDeploying } = useBurner();
 
-    const [newAccount, setNewAccount] = useState(false);
+  const [newAccount, setNewAccount] = useState(false);
 
-    useEffect(() => {
-        console.log("new account created")
-        setNewAccount(!newAccount);
+  useEffect(() => {
+    console.log("new account created");
+    setNewAccount(!newAccount);
+  }, [isDeploying]);
 
-
-    },[isDeploying])
-
-    return <StarknetConfig
-        connectors={[...connectors, ...arcadeAccounts]}
-        autoConnect
-    >
-        <ApolloProvider client={client}>{children}</ApolloProvider>
+  return (
+    <StarknetConfig connectors={[...connectors, ...arcadeAccounts]} autoConnect>
+      <ApolloProvider client={client}>{children}</ApolloProvider>
     </StarknetConfig>
+  );
 }
