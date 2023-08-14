19431a7061bd73616f5c6d2872e2700a7ae6a854
starknetdev <starknetdev@gmail.com>
2023-08-14 16:01:13 +0100

    handle regex for obstacles

--- ui/src/app/components/actions/DiscoveryDisplay.tsx
+++ ui/src/app/components/actions/DiscoveryDisplay.tsx
@@ -58,7 +58,13 @@ export const DiscoveryDisplay = ({ discoveryData }: DiscoveryProps) => {
       if (discoveryData?.dodgedObstacle) {
         return (
           <span className="flex flex-row items-center justify-between">
-            <p>PHEW! You avoided the {discoveryData?.obstacle}!</p>
+            <p>
+              PHEW! You avoided{" "}
+              {discoveryData?.obstacle?.endsWith("s")
+                ? discoveryData?.obstacle
+                : `the ${discoveryData?.obstacle}`}
+              !
+            </p>
             <SpikedWallIcon />
           </span>
         );
