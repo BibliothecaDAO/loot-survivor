a8a55e0e2abd1f44d31bdbeac693251e9955580c
starknetdev <starknetdev@gmail.com>
2023-09-05 21:54:32 +0100

    udate ports

--- new-indexer/scripts/run_discoveries.sh
+++ new-indexer/scripts/run_discoveries.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  apibara run --allow-env=env src/discoveries.ts -A dna_jX3t04zs9zywBnHWVmUq --status-server-address 0.0.0.0:1001 --persist-to-fs=.apibara --sink-id=discoveries
+  apibara run --allow-env=env src/discoveries.ts -A dna_jX3t04zs9zywBnHWVmUq --status-server-address 0.0.0.0:1004 --persist-to-fs=.apibara --sink-id=discoveries
   echo "Apibara exited with status $? - Restarting..." >&2
   sleep 1
 done
