138c428aea2383e487554f224e85155f4e692978
starknetdev <starknetdev@gmail.com>
2023-09-05 21:49:46 +0100

    fix dockerfile

--- new-indexer/master_script.sh
+++ new-indexer/master_script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-./scripts/run_adventurers.sh &
-./scripts/run_battles.sh &
-./scripts/run_beasts.sh &
-./scripts/run_discoveries.sh &
-./scripts/run_items.sh &
+/scripts/run_adventurers.sh &
+/scripts/run_battles.sh &
+/scripts/run_beasts.sh &
+/scripts/run_discoveries.sh &
+/scripts/run_items.sh &
 
 # Wait for all background jobs to finish (should never happen in this case)
 wait
