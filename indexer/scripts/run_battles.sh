56f18f5bae2431a1ba6e0a7c8c338976737f6c1a
starknetdev <starknetdev@gmail.com>
2023-09-11 17:21:32 +0100

    fix env filename

--- indexer/scripts/run_battles.sh
+++ indexer/scripts/run_battles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  apibara run --allow-env=env src/battles.ts -A dna_jX3t04zs9zywBnHWVmUq --status-server-address 0.0.0.0:1002 --persist-to-fs=.apibara --sink-id=battles
+  apibara run --allow-env=env-goerli src/battles.ts -A dna_jX3t04zs9zywBnHWVmUq --status-server-address 0.0.0.0:1002 --persist-to-fs=.apibara --sink-id=battles
   echo "Apibara exited with status $? - Restarting..." >&2
   sleep 1
 done
--- indexer/scripts/run_scores.sh
+++ indexer/scripts/run_scores.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  apibara run --allow-env=env src/scores.ts -A dna_jX3t04zs9zywBnHWVmUq --status-server-address 0.0.0.0:1006 --persist-to-fs=.apibara --sink-id=scores
+  apibara run --allow-env=env-goerli src/scores.ts -A dna_jX3t04zs9zywBnHWVmUq --status-server-address 0.0.0.0:1006 --persist-to-fs=.apibara --sink-id=scores
   echo "Apibara exited with status $? - Restarting..." >&2
   sleep 1
 done
