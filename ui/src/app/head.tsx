d5f0101338613a11dc78484494e6448209399e31
starknetdev <starknetdev@gmail.com>
2023-09-26 16:15:33 +0100

    fix meta tags

--- ui/src/app/head.tsx
+++ ui/src/app/head.tsx
@@ -5,12 +5,11 @@ export default function Head() {
 
       <meta charSet="utf-8" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="author" content="Bibliothecadao" />
       <link
         rel="icon"
         href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪦</text></svg>"
       />
-      <meta property="og:site_name" content="Survive" />
+      <meta property="og:site_name" content="Loot Survivor" />
       <meta
         name="description"
         content="Loot Survivor is the first Play2Die game from Realms on Starknet."
@@ -31,9 +30,9 @@ export default function Head() {
         name="twitter:description"
         content="Loot Survivor is the first Play2Die game from Realms on Starknet."
       />
-      <meta name="twitter:siteId" content="1467726470533754880" />
+      <meta name="twitter:site:id" content="1467726470533754880" />
       <meta name="twitter:creator" content="@bibliothecadao" />
-      <meta name="twitter:creatorId" content="1467726470533754880" />
+      <meta name="twitter:creator:id" content="1467726470533754880" />
       <meta name="twitter:image" content="/scenes/intro/beast.png" />
     </>
   );
