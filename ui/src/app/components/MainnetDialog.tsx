cdff1fd408c717206ae66ec92a2e65d2c3d50c33
starknetdev <starknetdev@gmail.com>
2023-10-05 12:47:33 +0100

    improve mainnet message

--- ui/src/app/components/MainnetDialog.tsx
+++ ui/src/app/components/MainnetDialog.tsx
@@ -7,8 +7,10 @@ export const MainnetDialog = () => {
       <div className="fixed text-center sm:top-1/8 sm:left-1/8 sm:left-1/4 sm:w-3/4 sm:w-1/2 h-3/4 border-4 bg-terminal-black z-50 border-terminal-green p-4 overflow-y-auto">
         <h3 className="mt-4">Mainnet</h3>
         <p className="m-2 text-sm xl:text-xl 2xl:text-2xl">
-          You are on the mainnet site! We have not launched currently, please
-          use testnet.
+          Greetings, brave adventurer! Welcome to the mainnet realm, though it
+          be but a ghost town for now! Our grand launch is still in the forge,
+          so set your sights on the testnet shores for now, and sharpen your
+          survival skills!
         </p>
         <a href="https://goerli-survivor.realms.world">
           <Button>Go To Testnet</Button>
