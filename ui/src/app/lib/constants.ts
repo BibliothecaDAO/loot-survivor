bfd8055a25992a70508b01607bcc9b025e4a12a4
starknetdev <starknetdev@gmail.com>
2023-10-11 18:20:36 +0100

    raise fee check gas limit

--- ui/src/app/lib/constants.ts
+++ ui/src/app/lib/constants.ts
@@ -97,7 +97,7 @@ export const idleDeathPenaltyBlocks = 300;
 
 // UI PARAMS
 export const MIN_BALANCE = 10000000000000; // 0.00001ETH or $0.015
-export const FEE_CHECK_BALANCE = 100000000000000; // 0.0001ETH or $0.15
+export const FEE_CHECK_BALANCE = 250000000000000; // 0.00025ETH or $0.45
 
 export const deathMessages = [
   {
