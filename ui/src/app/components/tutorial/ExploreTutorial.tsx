db605f3fc2e1a1d0a5fce1238e9712b979d54d0d
loothero <loothero@protonmail.com>
2023-10-15 17:57:53 +0000

    explore mod1

--- ui/src/app/components/tutorial/ExploreTutorial.tsx
+++ ui/src/app/components/tutorial/ExploreTutorial.tsx
@@ -7,10 +7,12 @@ export const ExploreTutorial = () => {
       </p>
       <ul className="text-sm sm:text-lg">
         <li>
-          Beasts - There are 75 types of beasts, each with a fixed elemental type and tier. The level and health of the beasts will vary and scale based on the adventurer's level. When encountering a beast, it will attempt to ambush and attack before you have a chance to react. Your chance change of avoiding this ambush is based on your Adventurer's Wisdom. Defeating provides gold and xp for your adventurer and items, with stronger beasts yielding more substantial gold and xp.
+          Beasts - 75 different beasts with various levels and health will lock
+          you in a battle. Fight or Flee to survive!
         </li>
         <li>
-          Obstacles - The game features 75 unique obstacles, each with a fixed elemental type and tier. The level of the Obstacles will vary and scale based on the adventurer's level. Adventurer's will either dodge an obstacle or suffer damage with the chance of dodging it based on the Adventurer's Intelligence.
+          Obstacles - 75 different obstacles that will give damage unless they
+          are avoided!
         </li>
         <li>Gold - A random amount of gold will be added to your balance</li>
         <li>Health Potions - Instant injection of a random amount</li>
