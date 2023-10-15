bacf7fd4bc3bbd4ad931b3eb7efae3821cfc2be2
loothero <100039621+loothero@users.noreply.github.com>
2023-10-15 16:09:08 -0400

    polishing onboarding tutorials (#412)

--- ui/src/app/components/tutorial/StrategyTutorial.tsx
+++ ui/src/app/components/tutorial/StrategyTutorial.tsx
@@ -1,18 +1,18 @@
 export const StrategyTutorial = () => {
   return (
     <div className="flex flex-col gap-5 uppercase items-center text-center h-full">
-      <h3 className="mt-0">Strategies</h3>
+      <h3 className="mt-0">Basic Strategy</h3>
       <p className="sm:text-lg">
-        You can carry up to 19 items, dropping and swapping is key to a good
-        score. You will be able to do this in the adventurer card or inventory.
+        If you take damage in a location without any armor, the damage will be substantial. Consider getting a full set of armor early in the game, even if that means using lower tier armor.
       </p>
       <p className="sm:text-lg">
-        You will spawn with 9 random stats, then it is up to you to decide your
-        build! Each stat has an importance to survival, there is no rule book.
+        Having multiple sets of armor with different elemental types allows you to maintain maximum elemental protection when battling beasts. Switching items during battle will result in the beast counter attacking it&apos;s best to minimize in-battle equipment changes.
       </p>
       <p className="sm:text-lg">
-        Think but be quick, there isn&apos;t much time before the idle death
-        penalty will get you!
+        Dexterity, Intelligence, and Wisdom help you avoid taking damage. The chance of fleeing a beast is governed by (dexterity / adventurer_level). If you have 1 dexterity and you are on level 10, your chance of fleeing a beast is 1/10. The same applies for Intelligence when dodging obstacles and Wisdom when avoiding beast ambushes.
+      </p>
+      <p className="sm:text-lg">
+        Adventurer&apos;s will receive a significant boost to their stats when their items reach greatness 15. Use this time wisely to prepare for the late-game where beasts will grow exponentially more powerful.
       </p>
     </div>
   );
--- ui/src/app/components/tutorial/UpgradeTutorial.tsx
+++ ui/src/app/components/tutorial/UpgradeTutorial.tsx
@@ -1,29 +1,27 @@
 export const UpgradeTutorial = () => {
   return (
     <div className="flex flex-col uppercase items-center text-center h-full">
-      <h3 className="mt-0">Upgrades</h3>
+      <h3 className="mt-0">Adventurer Upgrades</h3>
       <p className="sm:text-lg">
-        Whenever your adventurer levels up they will be able to upgrade and
-        boost their power!
+        Each time an Adventurer reaches a new level, they get the option to ugprade stats and purchase items and health potions.
       </p>
       <div className="flex flex-col gap-2">
         <div className="flex flex-col">
-          <p className="text-lg sm:text-2xl">Upgrade Stats</p>
+          <p className="text-lg sm:text-2xl">Stats Upgrades</p>
           <p className="sm:text-lg">
-            Adventurer can choose to upgrade 6 different stats.
+            Adventurer&apos;s receive one stat upgrade each time they level up.
           </p>
         </div>
         <div className="flex flex-col">
           <p className="text-lg sm:text-2xl">Health Potions</p>
           <p className="text-sm sm:text-lg">
-            Health potions give 10hp. Potion cost will rise as your journey
-            continues.
+            Health potions provide +10hp. The base cost of potions is equal to the adventurer&apos;s level. Charisma provides a +2 gold discount on potions.
           </p>
         </div>
         <div className="flex flex-col">
-          <p className="text-lg sm:text-2xl">Loot Fountain</p>
+          <p className="text-lg sm:text-2xl">Item Upgrades</p>
           <p className="sm:text-lg">
-            20 Loot Items will be available for each level.
+            Adventurer&apos;s will have the option to purchase new items each level up. The items in the market are random for each level up, with the inventory size based on the number of stat points available.
           </p>
         </div>
       </div>
