11c6cf54e9dac9b6d8be180dafcb9c3ec6823bb4
loothero <loothero@protonmail.com>
2023-10-15 19:46:45 +0000

    squash

--- ui/src/app/components/tutorial/ExploreTutorial.tsx
+++ ui/src/app/components/tutorial/ExploreTutorial.tsx
@@ -1,14 +1,14 @@
 export const ExploreTutorial = () => {
   return (
     <div className="flex flex-col gap-5 uppercase items-center text-center h-full">
-      <h3 className="mt-0">Exploring</h3>
+      <h3 className="mt-0">Explore</h3>
       <p className="text-sm sm:text-lg mb-2">
-        Exploring will yield one of three outcomes:
+        During your exploration, you will discovery many things. They include:
       </p>
       <ul className="text-sm sm:text-lg">
         <li>
-          Beasts: There are 75 beasts, each with a fixed tier and elemental type. 
-          The level and health of the beasts will vary based on the adventurer's level.
+          Beasts - 75 different beasts with various levels and health will lock
+          you in a battle. Fight or Flee to survive!
         </li>
         <li>
           Obstacles - 75 different obstacles that will give damage unless they
