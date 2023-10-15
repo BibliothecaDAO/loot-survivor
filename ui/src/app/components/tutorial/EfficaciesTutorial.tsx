420853a5c2db03b564b024ad368018478934ab03
loothero <loothero@protonmail.com>
2023-10-15 16:57:20 +0000

    Revert "polishing onboarding tutorials"
    This reverts commit c54f0b3a14616b2adaff116da8bff921ae12675d.

--- ui/src/app/components/tutorial/EfficaciesTutorial.tsx
+++ ui/src/app/components/tutorial/EfficaciesTutorial.tsx
@@ -3,10 +3,11 @@ import { efficacyData } from "@/app/lib/constants";
 export const EfficacyHint = () => {
   return (
     <div className="flex flex-col gap-2 uppercase items-center text-center h-full">
-      <h3 className="mt-0 uppercase">Elemental</h3>
+      <h3 className="mt-0 uppercase">Efficacies</h3>
       <div className="flex flex-col gap-5">
         <p className="sm:text-2xl">
-          Understanding weapon and armor elemental is vital to survival!
+          Understanding the efficacy chart is vital for surviving through
+          combat!
         </p>
         <table className="uppercase whitespace-nowrap border border-terminal-green text-sm">
           <thead>
