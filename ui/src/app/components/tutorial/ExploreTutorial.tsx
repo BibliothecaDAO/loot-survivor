e06e4f0ebec72cf86c613c48c4230b59635cd3e7
starknetdev <starknetdev@gmail.com>
2023-10-16 15:19:49 +0100

    minor design changes to hints

--- ui/src/app/components/tutorial/ExploreTutorial.tsx
+++ ui/src/app/components/tutorial/ExploreTutorial.tsx
@@ -3,20 +3,20 @@ export const ExploreTutorial = () => {
     <div className="flex flex-col gap-5 uppercase items-center text-center h-full">
       <h3 className="mt-0">Exploration</h3>
 
-      <p className="text-sm sm:text-lg mb-2">
+      <p className="text-sm sm:text-xl mb-2">
         When you go exploring, you will encounter:
       </p>
 
-      <ul className="text-sm sm:text-lg">
-        <li>
-          Beasts with a wide range of health and power. Choose your battles carefully.
+      <ul className="text-sm sm:text-xl">
+        <li className="mb-5">
+          Beasts with a wide range of health and power. Choose your battles
+          carefully.
         </li>
-        <li>
-          Obstacles with the potential to deal fatal damage. Increase your chance of dodging obstacles by upgrading intelligence. 
+        <li className="mb-5">
+          Obstacles with the potential to deal fatal damage. Increase your
+          chance of dodging obstacles by upgrading intelligence.
         </li>
-        <li>
-          Gold and Health
-        </li>
+        <li>Gold and Health</li>
       </ul>
     </div>
   );
--- ui/src/app/components/tutorial/ItemSpecialsTutorial.tsx
+++ ui/src/app/components/tutorial/ItemSpecialsTutorial.tsx
@@ -1,23 +1,29 @@
 export const UnlocksTutorial = () => {
   return (
-    <div className="flex flex-col gap-2 uppercase items-center text-center h-full">
+    <div className="flex flex-col gap-5 uppercase items-center text-center h-full">
       <h3 className="mt-0">Item Specials</h3>
 
       <p className="sm:text-lg">
-        Loot items receive special abilities when they reach greatness 15, 19, and 20.
+        Loot items receive special abilities when they reach greatness 15, 19,
+        and 20.
       </p>
+      <ul>
+        <li className="sm:text-lg mb-2">
+          G15: They receive a Special suffix such as &quot;Of Power&quot;. There
+          are sixteen suffixes, each providing a unique set of stat boosts.
+        </li>
 
-      <p className="sm:text-lg">
-        At G15, they receive a Special suffix such as &quot;Of Power&quot;. There are sixteen suffixes, each providing a unique set of stat boosts.
-      </p>
+        <li className="sm:text-lg mb-2">
+          G19: They receive a two part name prefix such as &quot;Agony
+          Bane&quot; which if it matches a beast, provides a powerful damage
+          boost.
+        </li>
 
-      <p className="sm:text-lg">
-        At G19, they receive a two part name prefix such as &quot;Agony Bane&quot; which if it matches a beast, provides a powerful damage boost.
-      </p>
-
-      <p className="sm:text-lg">
-        At G20, they receive a +1 modifier which grants the player a stat upgrade for their Adventurer.
-      </p>
+        <li className="sm:text-lg">
+          G20: They receive a +1 modifier which grants the player a stat upgrade
+          for their Adventurer.
+        </li>
+      </ul>
     </div>
   );
 };
