f59f7d8102d1ccfc642d7827b1c06e48f43789cc
starknetdev <starknetdev@gmail.com>
2023-10-17 15:10:03 +0100

    add trx error response

--- ui/src/app/lib/utils/syscalls.ts
+++ ui/src/app/lib/utils/syscalls.ts
@@ -2,6 +2,7 @@ import {
   InvokeTransactionReceiptResponse,
   Call,
   AccountInterface,
+  RevertedTransactionReceiptResponse,
 } from "starknet";
 import { GameData } from "@/app/lib/data/GameData";
 import {
@@ -207,7 +208,7 @@ export function syscalls({
       calldata: [
         "0x0628d41075659afebfc27aa2aab36237b08ee0b112debd01e56d037f64f6082a",
         getKeyFromValue(gameData.ITEMS, formData.startingWeapon) ?? "",
-        stringToFelt(formData.name).toString(),
+        // stringToFelt(formData.name).toString(),
       ],
     };
 
@@ -229,6 +230,17 @@ export function syscalls({
         undefined
       );
       try {
+        throw new Error(`Error in the called contract (0x010515a855de4d9a1db962fe18c20ff2245e5cc6c6982f2b41a6e4af0de6d544):
+          Error at pc=0:6482:
+          Got an exception while executing a hint: Custom Hint Error: Execution failed. Failure reason: "Failed to deserialize param #3".
+          Cairo traceback (most recent call last):
+          Unknown location (pc=0:634)
+          Unknown location (pc=0:2959)
+          Unknown location (pc=0:4287)
+          Unknown location (pc=0:5609)
+          Unknown location (pc=0:5609)
+          Unknown location (pc=0:5594)
+        `);
         const tx = await handleSubmitCalls(account, [
           ...calls,
           mintLords,
@@ -248,6 +260,16 @@ export function syscalls({
             retryInterval: 2000,
           }
         );
+        console.log(receipt);
+        // Handle if the tx was reverted
+        if (
+          (receipt as RevertedTransactionReceiptResponse).execution_status ===
+          "REVERTED"
+        ) {
+          throw new Error(
+            (receipt as RevertedTransactionReceiptResponse).revert_reason
+          );
+        }
         // Here we need to process the StartGame event first and use the output for AmbushedByBeast event
         const startGameEvents = await parseEvents(
           receipt as InvokeTransactionReceiptResponse,
