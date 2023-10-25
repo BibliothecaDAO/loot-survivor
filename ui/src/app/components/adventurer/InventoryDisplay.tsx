a727c6ea426fbaa585d7697976f177b28d040485
starknetdev <starknetdev@gmail.com>
2023-10-25 15:44:06 +0100

    make equip switches display responsive

--- ui/src/app/components/adventurer/InventoryDisplay.tsx
+++ ui/src/app/components/adventurer/InventoryDisplay.tsx
@@ -31,9 +31,9 @@ export const InventoryDisplay = ({
   gameContract,
 }: InventoryDisplayProps) => {
   return (
-    <div className="flex flex-row items-center justify-between w-full">
+    <div className="flex flex-row items-center justify-between w-full h-full">
       {itemsOwnedInSlot.length > 0 ? (
-        <div className="flex flex-row gap-1 overflow-x-auto">
+        <div className="flex flex-row gap-1 overflow-x-auto item-scroll h-full">
           {itemsOwnedInSlot.map((item, index) => (
             <InventoryCard
               itemSlot={itemSlot}
@@ -113,35 +113,41 @@ export const InventoryCard = ({
   };
 
   return (
-    <div className="flex flex-row items-center justify-between w-1/2 border border-terminal-green p-1 w-full">
+    <div className="flex flex-row items-center justify-between border border-terminal-green p-1">
       <div className="flex flex-row items-center">
-        <div className="sm:hidden flex flex-col items-center justify-center border-r-2 border-terminal-black p-1 sm:p-2 gap-2">
+        <div className="sm:hidden flex flex-col items-center justify-center p-1 sm:p-2 gap-2">
           <LootIcon size={"w-3"} type={itemSlot ? itemSlot : slot} />
           <Efficacyicon size={"w-3"} type={type} />
         </div>
-        <div className="hidden sm:flex flex-col justify-center border-r-2 border-terminal-black p-1 sm:p-2 gap-2">
+        <div className="hidden sm:flex flex-col justify-center p-1 sm:p-2 gap-2">
           <LootIcon size={"w-4"} type={itemSlot ? itemSlot : slot} />
           <Efficacyicon size={"w-4"} type={type} />
         </div>
-        <span className="flex flex-col gap-1 whitespace-nowrap text-sm sm:text-xxs md:text-lg">
-          <div className="flex flex-row font-semibold text-xs space-x-3">
+        <span className="flex flex-col sm:gap-1 whitespace-nowrap text-sm sm:text-xxs md:text-lg">
+          <div className="flex flex-row font-semibold items-center text-xs space-x-3">
             <span className="self-center">
               {item &&
                 `Tier ${tier ?? 0}
                   `}
             </span>
             <span>Greatness {calculateLevel(item?.xp ?? 0)}</span>
+            {/* <Button
+              size={"xxxs"}
+              onClick={() => handleEquipItems(item?.item ?? "")}
+              disabled={equipItems.includes(itemId)}
+            > */}
+            <button
+              onClick={() => handleEquipItems(item?.item ?? "")}
+              disabled={equipItems.includes(itemId)}
+              className="bg-terminal-green text-terminal-black px-1 sm:p-1 uppercase disabled:bg-terminal-black disabled:text-terminal-green"
+            >
+              <p className="text-xxs sm:text-xs">Equip</p>
+            </button>
+            {/* </Button> */}
           </div>
           <p className="text-text-ellipsis">{itemName}</p>
         </span>
       </div>
-      <Button
-        size={"xxs"}
-        onClick={() => handleEquipItems(item?.item ?? "")}
-        disabled={equipItems.includes(itemId)}
-      >
-        Equip
-      </Button>
     </div>
   );
 };
