d340713577a9de497b0a676e7115ba6d391fa6c4
starknetdev <starknetdev@gmail.com>
2023-10-28 19:18:49 +0100

    improve error styling

--- ui/src/app/error.tsx
+++ ui/src/app/error.tsx
@@ -1,23 +1,34 @@
 "use client";
 
 import Image from "next/image";
+import { Button } from "./components/buttons/Button";
+import { DiscordIcon } from "./components/icons/Icons";
 
 export default function ErrorPage() {
   return (
     <div>
       <Image src={"/scenes/intro/opening.png"} alt="Error Page" fill={true} />
       <div className="absolute inset-0 bg-black opacity-50" />
-      <div className="fixed inset-0 flex flex-col items-center justify-center text-center h-full gap-10">
+      <div className="fixed inset-0 flex flex-col items-center justify-center text-center h-full gap-10 p-20">
         <p className="sm:text-6xl text-terminal-yellow">
           ERROR: You have been vanquished by an unexpected bug!
         </p>
-        <p className="sm:text-2xl">
+        <p className="sm:text-3xl">
           You have strayed into the accursed realm of Errors and Glitches. Seek
           the guidance of a wise developer to overcome this treacherous path.
         </p>
         <p className="sm:text-2xl">
           May the code be with you, brave adventurer!
         </p>
+        <a
+          href="https://discord.gg/bibliothecadao"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button className="py-2 px-4 animate-pulse hidden sm:flex flex-row gap-2">
+            Go to Discord <DiscordIcon className="w-5" />
+          </Button>
+        </a>
       </div>
     </div>
   );
