38d0d4dcd306aa0f6e0daaa979bafde63e947e7d
starknetdev <starknetdev@gmail.com>
2023-10-28 18:01:46 +0100

    test error

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -459,8 +459,6 @@ function Home({ updateConnectors }: HomeProps) {
     }
   };
 
-  throw new Error("test");
-
   return (
     <main
       className={`min-h-screen container mx-auto flex flex-col sm:pt-8 sm:p-8 lg:p-10 2xl:p-20 `}
