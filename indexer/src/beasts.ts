3b23c272732035d4f81f91cd92535043805a0d11
starknetdev <starknetdev@gmail.com>
2023-10-31 21:02:15 +0000

    update db to env variable

--- indexer/src/beasts.ts
+++ indexer/src/beasts.ts
@@ -18,6 +18,7 @@ import { MONGO_CONNECTION_STRING } from "./utils/constants.ts";
 const GAME = Deno.env.get("GAME");
 const START = +(Deno.env.get("START") || 0);
 const STREAM_URL = Deno.env.get("STREAM_URL");
+const MONGO_DB = Deno.env.get("MONGO_DB");
 
 const filter = {
   header: { weak: true },
@@ -38,7 +39,7 @@ export const config: Config<Starknet, Mongo | Console> = {
   sinkType: "mongo",
   sinkOptions: {
     connectionString: MONGO_CONNECTION_STRING,
-    database: "mongo_goerli",
+    database: MONGO_DB,
     collectionName: "beasts",
     // @ts-ignore - indexer package not updated
     entityMode: true,
