2b612b327c360a8f19454de04e75c6064232f354
starknetdev <starknetdev@gmail.com>
2023-11-01 15:26:42 +0000

    update discord link

--- ui/src/app/containers/GuideScreen.tsx
+++ ui/src/app/containers/GuideScreen.tsx
@@ -112,17 +112,22 @@ export default function GuideScreen() {
   return (
     <div className="overflow-y-auto p-2 table-scroll text-xs sm:text-base sm:text-left h-full">
       <div className="flex justify-center items-center mb-4 self-center flex-col">
-        <Button size={"lg"} href="https://survivor-docs.realms.world/">
-          Survivor Docs
-        </Button>
-        <Button
-          href="https://discord.gg/bibliothecadao"
+        <a
+          href="https://survivor-docs.realms.world/"
           target="_blank"
           rel="noopener noreferrer"
-          className="py-2 px-4 animate-pulse hidden sm:flex flex-row gap-2 mt-8"
         >
-          Join the Discord <DiscordIcon className="w-5" />
-        </Button>
+          <Button size={"lg"}>Survivor Docs</Button>
+        </a>
+        <a
+          href="https://discord.gg/realmsworld"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Button className="py-2 px-4 animate-pulse hidden sm:flex flex-row gap-2 mt-8">
+            Join the Discord <DiscordIcon className="w-5" />
+          </Button>
+        </a>
       </div>
       {/* <div className="">
         <h3 className="text-center text-l mb-2">
