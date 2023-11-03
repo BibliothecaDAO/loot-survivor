ac2f8fab0c8b8165931d4346247962ae10a55844
spaghettiOnToast <starknetgg@gmail.com>
2023-11-03 01:15:10 +0000

    fix error message

--- ui/src/app/error.tsx
+++ ui/src/app/error.tsx
@@ -11,11 +11,12 @@ export default function ErrorPage() {
       <div className="absolute inset-0 bg-black opacity-50" />
       <div className="fixed inset-0 flex flex-col items-center justify-center text-center h-full gap-10 p-20">
         <p className="sm:text-6xl text-terminal-yellow">
-          ERROR: You have been vanquished by an unexpected bug!
+          ERROR: You have been hit by an unexpected bug!
         </p>
         <p className="sm:text-3xl">
-          You have strayed into the accursed realm of Errors and Glitches. Seek
-          the guidance of a wise developer to overcome this treacherous path.
+          You have strayed into the accursed realm of Errors and Glitches.
+          Refresh or seek the guidance of a wise developer to overcome this
+          treacherous path.
         </p>
         <p className="sm:text-2xl">
           May the code be with you, brave adventurer!
