fa9f956ba2b09d703052c46b33e45fe2559a518d
starknetdev <starknetdev@gmail.com>
2023-11-09 12:23:41 +0000

    fix build

--- ui/src/app/components/CountDown.tsx
+++ ui/src/app/components/CountDown.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useMemo } from "react";
+import { useEffect, useRef } from "react";
 import { useCountUp } from "react-countup";
 import { useState } from "react";
 import { penaltyTime } from "../lib/constants";
