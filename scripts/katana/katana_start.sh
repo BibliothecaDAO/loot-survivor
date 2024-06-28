d0d7ef30a4aecb845f548478d685415ff49befb4
= <=>
2024-06-28 17:32:28 +0000

    squash me

--- scripts/katana/katana_start.sh
+++ scripts/katana/katana_start.sh
@@ -45,7 +45,7 @@ starkli account fetch --force --output /katana/starkli_account $account_address
 game_class_hash=$(starkli declare --watch /root/loot-survivor/target/dev/game_Game.contract_class.json --private-key $private_key --compiler-version 2.6.2 2>/dev/null)
 
 # deploy contract
-game_contract=$(starkli deploy --watch $game_class_hash $lords_address $dao_address $beasts_address $golden_token_address $terminal_timestamp $randomness_contract $randomness_rotation_interval $oracle_address $previous_first_place_address $previous_second_place_address $previous_third_place_address --private-key $private_key --max-fee 0.01 2>/dev/null)
+game_contract=$(starkli deploy --watch $game_class_hash $lords_address $dao_address $pg_address $beasts_address $golden_token_address $terminal_timestamp $randomness_contract $randomness_rotation_interval $oracle_address $previous_first_place_address $previous_second_place_address $previous_third_place_address --private-key $private_key --max-fee 0.01 2>/dev/null)
 
 # Export game_contract and private key and publish to .bashrc
 export game_contract
