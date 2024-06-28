d46fb0845c6330a60e3d843b85a19cc92f29076c
= <=>
2024-06-28 17:14:58 +0000

    update katana deployment for new constructor params

--- scripts/katana/katana_start.sh
+++ scripts/katana/katana_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # clear out previous .env file
-echo '' > /katana/.env
+echo '' >/katana/.env
 
 # source bashrc
 source ~/.bashrc
 
 # Start katana in the background
-/root/.dojo/bin/katana --disable-fee --allowed-origins "*" --block-time 10000 --seed 0x6c6f6f747375727669766f72 --accounts 10 --json-log --db-dir /katana/db > ~/katana.log 2>&1 &
+/root/.dojo/bin/katana --disable-fee --allowed-origins "*" --block-time 10000 --seed 0x6c6f6f747375727669766f72 --accounts 10 --json-log --db-dir /katana/db >~/katana.log 2>&1 &
 
 # Give katana a few seconds to initialize and log seed accounts
 sleep 3
@@ -26,6 +26,9 @@ terminal_timestamp=0
 randomness_contract=0
 randomness_rotation_interval=1
 oracle_address=0
+previous_first_place_address=0x02cd97240db3f679de98a729ae91eb996cab9fd92a9a578df11a72f49be1c356
+previous_second_place_address=0x02cd97240db3f679de98a729ae91eb996cab9fd92a9a578df11a72f49be1c356
+previous_third_place_address=0x02cd97240db3f679de98a729ae91eb996cab9fd92a9a578df11a72f49be1c356
 
 # extract account details from katana logs
 output=$(head -n 1 ~/katana.log | jq -r '.fields.message | fromjson | .accounts[0] | .[0], .[-1].private_key')
@@ -41,31 +44,31 @@ starkli account fetch --force --output /katana/starkli_account $account_address
 game_class_hash=$(starkli declare --watch /root/loot-survivor/target/dev/game_Game.contract_class.json --private-key $private_key --compiler-version 2.6.2 2>/dev/null)
 
 # deploy contract
-game_contract=$(starkli deploy --watch $game_class_hash $lords_address $dao_address $beasts_address $golden_token_address $terminal_timestamp $randomness_contract $randomness_rotation_interval $oracle_address --private-key $private_key --max-fee 0.01 2>/dev/null)
+game_contract=$(starkli deploy --watch $game_class_hash $lords_address $dao_address $beasts_address $golden_token_address $terminal_timestamp $randomness_contract $randomness_rotation_interval $oracle_address $previous_first_place_address $previous_second_place_address $previous_third_place_address --private-key $private_key --max-fee 0.01 2>/dev/null)
 
 # Export game_contract and private key and publish to .bashrc
 export game_contract
 export private_key
-echo "export game_contract=$game_contract" >> ~/.bashrc
-echo "export private_key=$private_key" >> ~/.bashrc
+echo "export game_contract=$game_contract" >>~/.bashrc
+echo "export private_key=$private_key" >>~/.bashrc
 
 # Append the game_contract value to katana.log
-echo "Game contract deployed at: $game_contract" >> ~/katana.log
+echo "Game contract deployed at: $game_contract" >>~/katana.log
 
 # write account, private key, and game contract to shared volume for easy access from hos
-echo "$account_address" > /katana/katana_account
-echo "$private_key" > /katana/private_key
-echo "$game_contract" > /katana/game_contract
+echo "$account_address" >/katana/katana_account
+echo "$private_key" >/katana/private_key
+echo "$game_contract" >/katana/game_contract
 
 # # Write environment variables to /env-katana
-echo 'MONGO_CONNECTION_STRING="mongodb://test:test@mongo:27017"' > /katana/.env
-echo 'MONGO_DB="mongo"' >> /katana/.env
-echo 'AUTH_TOKEN="dna_jX3t04zs9zywBnHWVmUq"' >> /katana/.env
-echo 'MONGO_USERNAME="test"' >> /katana/.env
-echo 'MONGO_PASSWORD="test"' >> /katana/.env
-echo 'STREAM_URL="http://dna:7171"' >> /katana/.env
-echo 'START=0' >> /katana/.env
-echo "GAME=$game_contract" >> /katana/.env
+echo 'MONGO_CONNECTION_STRING="mongodb://test:test@mongo:27017"' >/katana/.env
+echo 'MONGO_DB="mongo"' >>/katana/.env
+echo 'AUTH_TOKEN="dna_jX3t04zs9zywBnHWVmUq"' >>/katana/.env
+echo 'MONGO_USERNAME="test"' >>/katana/.env
+echo 'MONGO_PASSWORD="test"' >>/katana/.env
+echo 'STREAM_URL="http://dna:7171"' >>/katana/.env
+echo 'START=0' >>/katana/.env
+echo "GAME=$game_contract" >>/katana/.env
 
 # Keep the script running
 tail -f ~/katana.log
