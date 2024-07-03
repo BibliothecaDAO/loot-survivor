f1b49ce95b179c69ebfb2308e217fb8267c9d4aa
Starknet Dev <starknetdev@gmail.com>
2024-07-03 21:27:22 +0100

    - update favicon - update title - update description - update pwa icons

--- ui/src/app/head.tsx
+++ ui/src/app/head.tsx
@@ -1,24 +1,32 @@
 export default function Head() {
   return (
     <>
-      <title>Loot Survivor 💀</title>
+      <title>Loot Survivor</title>
 
       <meta charSet="utf-8" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link
         rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪦</text></svg>"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
       />
       <meta property="og:site_name" content="Loot Survivor" />
       <meta
         name="description"
-        content="Loot Survivor is the first Play2Die game from Realms on Starknet."
+        content="Loot Survivor is a fully on-chain arcade dungeon crawler game on Starknet."
       />
       <meta property="og:title" content="Loot Survivor" />
       <meta property="og:type" content="website" />
       <meta
         property="og:description"
-        content="Loot Survivor is the first Play2Die game from Realms on Starknet."
+        content="Loot Survivor is a fully on-chain arcade dungeon crawler game on Starknet."
       />
       <meta property="og:url" content="https://survivor.realms.world" />
       <meta
@@ -34,7 +42,7 @@ export default function Head() {
       <meta name="twitter:title" content="Loot Survivor" />
       <meta
         name="twitter:description"
-        content="Loot Survivor is the first Play2Die game from Realms on Starknet."
+        content="Loot Survivor is a fully on-chain arcade dungeon crawler game on Starknet."
       />
       <meta name="twitter:site:id" content="1467726470533754880" />
       <meta name="twitter:creator" content="@bibliothecadao" />
