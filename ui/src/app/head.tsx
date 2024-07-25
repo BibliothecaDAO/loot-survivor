d9070d8def53d1a530ddabf2ec1422be812090ec
Starknet Dev <starknetdev@gmail.com>
2024-07-25 17:08:30 +0100

    add player meta tags for slink

--- ui/src/app/head.tsx
+++ ui/src/app/head.tsx
@@ -44,14 +44,14 @@ export default function Head() {
         name="twitter:description"
         content="Loot Survivor is a fully on-chain arcade dungeon crawler game on Starknet."
       />
-      <meta name="twitter:site:id" content="1467726470533754880" />
-      <meta name="twitter:creator" content="@bibliothecadao" />
-      <meta name="twitter:creator:id" content="1467726470533754880" />
+      <meta name="twitter:site:id" content="@lootsurvivor" />
       <meta
         name="twitter:image"
         content="https://survivor.realms.world/scenes/intro/beast.png"
       />
-
+      <meta name="twitter:player" content="https://lootsurvivor.io/" />
+      <meta name="twitter:player:width" content="360" />
+      <meta name="twitter:player:height" content="560" />
       {/* Robots data */}
       <meta name="robots" content="noindex, follow, nocache" />
       <meta
